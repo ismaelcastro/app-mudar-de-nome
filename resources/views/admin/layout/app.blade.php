35c60e4c7779ecb56a4a2de574f28818300e50a0
Romulo Sousa <romulo.sousa@pravler.com.br>
2022-08-10 20:20:03 -0300

    ADD: adcionando a rota para a página de contatos

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -299,7 +299,7 @@
                                 </span>
                                 <div class="menu-sub menu-sub-accordion">
                                     <div class="menu-item">
-                                        <a class="menu-link" href="#">
+                                        <a class="menu-link" href="{{ route('admin.clients.index') }}">
                                             <span class="menu-bullet">
                                                 <span class="bullet bullet-dot"></span>
                                             </span>
