da3ac0fa2ff25b7a34a67f2bc28b3857bbaf4367
Romulo Sousa <romulo.sousa@pravler.com.br>
2022-08-16 22:34:25 -0300

    ADD: script de animação para atualizar o email e senha do usuário FIX: otimização da importação dos SVG usando o file_get_contents

--- resources/views/admin/pages/clients/show.blade.php
+++ resources/views/admin/pages/clients/show.blade.php
@@ -61,14 +61,14 @@
                     <!--begin::Navs-->
                     <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bold" role="tablist">
                         <li class="nav-item mt-2" role="presentation">
-                            <a class="nav-link text-active-primary ms-0 me-10 py-5 active"
+                            <a class="nav-link text-active-primary ms-0 me-10 py-5 active" id="kt_visao_geral_btn"
                                data-bs-toggle="tab" href="#kt_visao_geral" aria-selected="false" role="tab" tabindex="-1"
                             >Visão Geral</a>
                         </li>
                         <li class="nav-item mt-2" role="presentation">
-                            <a class="nav-link text-active-primary ms-0 me-10 py-5"
-                               data-bs-toggle="tab" href="#kt_configuracoes" aria-selected="true" role="tab"
-                            >Configurações</a>
+                            <a id="kt_configuracoes_btn" class="nav-link text-active-primary ms-0 me-10 py-5"
+                               data-bs-toggle="tab" href="#kt_configuracoes" aria-selected="true" role="tab">
+                                Configurações</a>
                         </li>
                     </ul>
                     <!--begin::Navs-->
@@ -1039,7 +1039,7 @@
             </div>
 
             <!--begin::Sign-in Method-->
-            <div class="card mb-5 mb-xl-10">
+            <div class="card mb-5 mb-xl-10" id="kt_signin_method">
                 <!--begin::Card header-->
                 <div class="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_signin_method">
                     <div class="card-title m-0">
@@ -1146,14 +1146,7 @@
                         <!--begin::Notice-->
                         <div class="notice d-flex bg-light-primary rounded border-primary border border-dashed p-6">
                             <!--begin::Icon-->
-                            <!--begin::Svg Icon | path: icons/duotune/general/gen048.svg-->
-                            <span class="svg-icon svg-icon-2tx svg-icon-primary me-4">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path opacity="0.3" d="M20.5543 4.37824L12.1798 2.02473C12.0626 1.99176 11.9376 1.99176 11.8203 2.02473L3.44572 4.37824C3.18118 4.45258 3 4.6807 3 4.93945V13.569C3 14.6914 3.48509 15.8404 4.4417 16.984C5.17231 17.8575 6.18314 18.7345 7.446 19.5909C9.56752 21.0295 11.6566 21.912 11.7445 21.9488C11.8258 21.9829 11.9129 22 12.0001 22C12.0872 22 12.1744 21.983 12.2557 21.9488C12.3435 21.912 14.4326 21.0295 16.5541 19.5909C17.8169 18.7345 18.8277 17.8575 19.5584 16.984C20.515 15.8404 21 14.6914 21 13.569V4.93945C21 4.6807 20.8189 4.45258 20.5543 4.37824Z" fill="currentColor" />
-                                        <path d="M10.5606 11.3042L9.57283 10.3018C9.28174 10.0065 8.80522 10.0065 8.51412 10.3018C8.22897 10.5912 8.22897 11.0559 8.51412 11.3452L10.4182 13.2773C10.8099 13.6747 11.451 13.6747 11.8427 13.2773L15.4859 9.58051C15.771 9.29117 15.771 8.82648 15.4859 8.53714C15.1948 8.24176 14.7183 8.24176 14.4272 8.53714L11.7002 11.3042C11.3869 11.6221 10.874 11.6221 10.5606 11.3042Z" fill="currentColor" />
-                                    </svg>
-                                </span>
-                            <!--end::Svg Icon-->
+                            {!! file_get_contents('metronic/media/icons/duotune/general/gen048.svg') !!}
                             <!--end::Icon-->
                             <!--begin::Wrapper-->
                             <div class="d-flex flex-stack flex-grow-1 flex-wrap flex-md-nowrap">
@@ -1183,4 +1176,5 @@
 @endsection
 
 @push('scripts')
+    <script src="{{ asset("metronic/js/custom/account/settings/signin-methods.js")}}"></script>
 @endpush
