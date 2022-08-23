fd5ddaf6d56aa1d251dcd76c690f7365806b7476
Romulo Sousa <romulo.sousa@pravler.com.br>
2022-08-23 11:49:11 -0300

    add: metodo de atualização do perfil, email e nome, padronização da classe

--- app/Http/Controllers/Client/ClientsController.php
+++ app/Http/Controllers/Client/ClientsController.php
@@ -2,34 +2,36 @@
 
 namespace App\Http\Controllers\Client;
 
+use App\Http\Requests\Client\UpdateProfileRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use App\Http\Controllers\Controller;
 use App\Models\Client;
 
 class ClientsController extends Controller
 {
-    public function change_mail(Request $request)
+    public function change_mail(Request $request): RedirectResponse
     {
         $client = auth('client')->user();
-        $data = $this->_validate_change_mail($request,$client->id);
+        $data = $this->_validate_change_mail($request, $client->id);
         $client->fill($data);
         $client->save();
-        return redirect()->back()->with('success','e-mail alterado!');
+        return redirect()->back()->with('success', 'e-mail alterado!');
     }
 
-    public function change_stagecall(Request $request)
+    public function change_stagecall(Request $request): RedirectResponse
     {
-        if(isset($request->clear)){
+        if (isset($request->clear)) {
             $stage = $request->stage;
             $client = auth('client')->user();
-            $call = $client->call()->orderBy('calls.id','desc')->first();
+            $call = $client->call()->orderBy('calls.id', 'desc')->first();
             $call->stage_call = $stage;
             $call->save();
         }
         return redirect()->back();
     }
 
-    protected function _validate_change_mail(Request $request, $id)
+    protected function _validate_change_mail(Request $request, $id): array
     {
         return $this->validate($request, [
             'email' => "required|string|email|max:191|unique:clients,email,{$id},id"
@@ -41,7 +43,14 @@ class ClientsController extends Controller
         return view('client.pages.perfil.index');
     }
 
-    public function updateProfile(Request $request, Client $client)
+    public function updateProfile(UpdateProfileRequest $profileRequest, Client $client): RedirectResponse
+    {
+        $client->fill($profileRequest->validated())->save();
+
+        return redirect()->route('client.perfil')->with('success', 'Atualizado com sucesso');
+    }
+
+    public function updateAddress(Request $request, Client $client)
     {
         //
     }
